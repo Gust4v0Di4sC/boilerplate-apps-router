ec03bb5f54bd7eabbfcc6e74c1015b306455739b
Gustavo dias <dscharraa@gmail.com>
2025-11-27 21:28:44 -0300

    jest tests

---
+++ src/components/Main/styles.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import styled from "styled-components"
+
+export const Wrapper = styled.main`
+    background-color: #06092b;
+    color: #fff;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 3rem;
+    font-weight: bold;
+    padding: 2rem;
+    text-align: center;
+    width: 100%;
+    height: 100%;
+`
